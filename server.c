4bc505c1f8a0dc4f64828af274f693e343dba3c9
ClaudeManning <64705956+ClaudeManning@users.noreply.github.com>
2021-04-04 01:43:10 -0500

    Update server.c

--- server.c
+++ server.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]){
         printf("Received: %s\n",buf);
 
         printf("Enter messsage to send to client:" );
-        printf("To end communication enter shutdown");
+        printf("To end communication enter shutdown or quit");
         scanf("%s",msg);
 
         int bytes_sent=send(sock_recv,msg,BUF_SIZE,0);
